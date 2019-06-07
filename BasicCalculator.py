3efcaec49d1061702cc134086bb739c691580869
Lorenzo <51018693+lorenzobalestra@users.noreply.github.com>
2019-06-07 02:06:10 +0200

    Add files via upload

---
+++ BasicCalculator.py
@@ -0,0 +1,7 @@
+# Building a basic calculator in Python
+
+num1 = input("Insert an integer number: ")
+num2 = input("Insert another integer number: ")
+result = int(num1) + int(num2)
+
+print(result)
